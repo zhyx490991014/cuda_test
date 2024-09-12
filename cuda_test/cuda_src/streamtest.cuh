37dcc609e9b0316efd81984d744543450958d0ab
ZYX <74281356+zhyx490991014@users.noreply.github.com>
2024-09-12 16:55:31 +0800

    Delete cuda_test directory

--- cuda_test/cuda_src/streamtest.cuh
+++
@@ -1,7 +0,0 @@
-#include "cuda_runtime.h"
-#include "device_launch_parameters.h"
-#include <iostream>
-#include <stdio.h>
-
-__global__ void sayHello3(int N, int *a, int *b, int *c);
-int callforsayHello3();
